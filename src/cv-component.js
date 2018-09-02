03c54ea560e76e1b02806acbdc176dbd4e8cc526
Edu <a@a.a>
2018-09-02 19:48:08 +0200

    feat: yei! we have the head!

--- src/cv-component.js
+++ src/cv-component.js
@@ -2,33 +2,85 @@ import {LitElement, html} from '@polymer/lit-element';
 
 class cvComponent extends LitElement {
 
-  static get properties() {
-    return {
-      cvData: {type: Object}
-    };
-  }
+    static get properties() {
+        return {
+            cvData: { type: Object }
+        };
+    }
 
-  static get styles() {
-    return html`
-      <style>
-        body {
-          background-color: green;
+    constructor() {
+        super();
+        fetch('./cv.json')
+        .then(response => response.json())
+        .then(data => this.cvData = data);
+    }
+
+    _render() {
+        return html`
+        <style>
+            .personal-information {
+                background-color: #793323;
+                width: 100%;
+                box-sizing: border-box;
+                color: white;
+                padding: 1.5em;
+            }
+            .personal-information h1 {
+                text-align: center;
+                margin: 0;
+            }
+            .complete-name {
+                padding: 0.8em;
+            }
+            .profile-data-container {
+                margin-top: 0.5em;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: space-evenly;
+            }
+            .profile-data-container img {
+                height: 6em;
+                width: 6em;
+                border: 1px solid white;
+                border-radius: 50%;
+            }
+            .profile-data-container ul {
+                text-align: left;
+                font-size: 1.2em;
+                line-height: 1.8em;
+            }
+            .personal-introduction-container {
+                margin-top: 1.5em;
+                border: 1px dashed white;
+                padding: 0.7em;
+                text-align: justify;
+            }
+        </style>
+        ${this.cvData
+            ? html`
+                <div class="personal-information">
+                    <div class="complete-name">
+                        <h1>${this.cvData.name}</h1>
+                        <h1>${this.cvData.surname}</h1>
+                    </div>
+                    <div class="profile-data-container">
+                        <img class="profile-photo" src="${this.cvData.profilePhotoUrl}" alt="profile-photo">
+                        <ul>
+                            <li>${this.cvData.title}</li>
+                            <li>${this.cvData.email}</li>
+                        </ul>
+                    </div>
+                    <div class="personal-introduction-container">
+                        <span>${this.cvData.introduction}</span>
+                    </div>
+                </div>`
+            : html`
+                <span>Loading...</span>
+            `
         }
-      </style>
-    `;
-  }
-
-  constructor() {
-    super();
-    fetch('./cv.json')
-      .then(response => response.json().then( data => {debugger;this.cvData = data;}));
-  }
-
-  _render() {
-    return html`
-      Web Components are cool!
-    `;
-  }
+        `;
+    }
 }
 
 customElements.define('cv-component', cvComponent);
