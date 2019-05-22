2b496b3e759b2800bd6847ad69c6d24e24256d25
Eduardo David González Bermejo <edudbermejo@gmail.com>
2019-05-22 01:31:25 +0100

    feat: enable travis ci (#7)
    * feat: added package.json
    
    * feat: added travis and deploy script
    
    * fix: moved stage into a job
    
    * fix: travis pipeline
    
    * fix: missing package.json. Deleted node_modules but kept for deploying to master
    
    * fix: misspelled travis
    
    * fix: change remote
    
    * fix: added remote
    
    * fix: renamed remote
    
    * fix: fetching remote
    
    * debug: adding debug commands
    
    * debug: added git log
    
    * debug: may git branch show us the way
    
    * fix: fetching remotes
    
    * fix: typo
    
    * fix: pull instead of fetch
    
    * fix: specifying the remote to pull
    
    * debug: listing remotes
    
    * fix: trying creating new branch
    
    * fix: authenticating with token
    
    * feat: ready for merging with develop

---
+++ .scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+NEW_VERSION=$(npm version minor --commit-hooks=false)
+git commit -am "chore: updated versION ${NEW_VERSION} [skip ci]"
+git push 
+git push --tags
+
+rm -rf .scripts
+rm .travis.yml package.json package-lock.json .gitignore
+
+git add . 
+git commit -m "chore: clean and ready for prod"
+
+git remote add tokenized "https://${GITHUB_TOKEN}@github.com/edudbermejo/edudbermejo.github.io.git"
+git checkout -b master
+git push -f tokenized master
