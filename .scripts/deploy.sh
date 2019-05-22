6945bfd7d9fe9b55ea73a0cb9b2899d3d683b345
Eduardo David González Bermejo <edudbermejo@gmail.com>
2019-05-22 01:59:48 +0100

    fix: using tokenized branch for all pushes (#11)

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,7 +5,9 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
 npm version minor -m "chore: updated version v%s [skip ci]"
-git push origin HEAD:develop
+
+git remote add tokenized "https://${GITHUB_TOKEN}@github.com/edudbermejo/edudbermejo.github.io.git"
+git push tokenized HEAD:develop
 git push --tags
 
 rm -rf .scripts
@@ -14,6 +16,5 @@ rm .travis.yml package.json package-lock.json .gitignore
 git add . 
 git commit -m "chore: clean and ready for prod"
 
-git remote add tokenized "https://${GITHUB_TOKEN}@github.com/edudbermejo/edudbermejo.github.io.git"
 git checkout -b master
 git push -f tokenized master
