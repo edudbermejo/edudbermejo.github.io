46cbc4a9bf64bd12e9890a595de71a17dc5a7695
Eduardo David González <edudbermejo@gmail.com>
2019-05-22 01:30:23 +0100

    feat: ready for merging with develop

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,10 +4,10 @@ set -e
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-#NEW_VERSION=$(npm version minor --commit-hooks=false)
-#git commit -am "chore: updated versION ${NEW_VERSION} [skip ci]"
-#git push 
-#git push --tags
+NEW_VERSION=$(npm version minor --commit-hooks=false)
+git commit -am "chore: updated versION ${NEW_VERSION} [skip ci]"
+git push 
+git push --tags
 
 rm -rf .scripts
 rm .travis.yml package.json package-lock.json .gitignore
@@ -15,10 +15,6 @@ rm .travis.yml package.json package-lock.json .gitignore
 git add . 
 git commit -m "chore: clean and ready for prod"
 
-# git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-# git remote -v
-# git pull origin master
 git remote add tokenized "https://${GITHUB_TOKEN}@github.com/edudbermejo/edudbermejo.github.io.git"
-# git branch -a
 git checkout -b master
 git push -f tokenized master
